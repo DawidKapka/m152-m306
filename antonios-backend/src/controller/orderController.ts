65aa3e37d50f3f4b3c504df1c49c5759ce69556b
DawidKapka <kapkadawid6@gmail.com>
2023-06-13 15:02:48 +0200

    fix formatting

--- antonios-backend/src/controller/orderController.ts
+++ antonios-backend/src/controller/orderController.ts
@@ -3,16 +3,16 @@ import {Order} from "../types/Order";
 import {findOrder, findOrderItems, saveOrder, saveOrderItems} from "../model/orderModel";
 
 export const createOrder = (req: Request, res: Response): void => {
-       const order: Order = req.body;
+    const order: Order = req.body;
     saveOrder(order).then((orderId: number) => {
-           saveOrderItems(orderId, order.orderItems).then(() => {
-               res.status(200);
-               res.send({orderId});
-           });
-       }).catch((err: Error) => {
-           res.status(401)
-           res.send(err);
-     });
+       saveOrderItems(orderId, order.orderItems).then(() => {
+           res.status(200);
+           res.send({orderId});
+       });
+    }).catch((err: Error) => {
+        res.status(401)
+        res.send(err);
+    });
 }
 
 export const getOrder = (req: Request, res: Response): void => {
